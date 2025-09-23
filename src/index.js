21c131e1c7b288230cf959069f63fd5927048b60
gnad0722 <haidang25102005@gmail.com>
2025-09-23 09:24:55 +0700

    Not test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
